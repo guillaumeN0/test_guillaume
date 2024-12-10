9ea68e1c12c60242859545446c02a86d2beef446
guillaume <guillaume.guers@baobabsoluciones.es>
2024-12-10 13:30:09 +0100

    day 3 black

--- advent_3/solutions_3.py
+++ advent_3/solutions_3.py
@@ -1,21 +1,22 @@
-import csv,re
+import csv, re
 import numpy as np
-reports_list=[]
 
-with open("input.txt","r",encoding="utf-8") as f:
-    instruction=f.read()
-    
-instructions_list=re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)",instruction)
-s=0
-test=1
+reports_list = []
+
+with open("input.txt", "r", encoding="utf-8") as f:
+    instruction = f.read()
+
+instructions_list = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", instruction)
+s = 0
+test = 1
 for instruction in instructions_list:
     if "mul" in instruction:
-        if test==1:
-            numbers=re.findall(r"\d{1,3}",instruction)
-            s+=int(numbers[0])*int(numbers[1])
+        if test == 1:
+            numbers = re.findall(r"\d{1,3}", instruction)
+            s += int(numbers[0]) * int(numbers[1])
     elif instruction == "do()":
-        test=1
+        test = 1
     else:
-        test=0
-    
+        test = 0
+
 print(s)
